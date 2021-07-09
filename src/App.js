14142f57d1ba8639f7c5c5bc55b04394a476fc9f
Your Nameeli <elighidiu@yahoo.com>
2021-07-09 10:15:51 +0200

    retrieve todos

--- src/App.js
+++ src/App.js
@@ -5,29 +5,42 @@ import Header from './components/Header';
 import Form from './components/Form';
 import List from './components/List';
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-let count = 0; 
+let count = 0;
+const LSKEY = "MyTodoApp";
 
 function App() {
 
   const initialTodos = [
-    { id:count++, task: "Learn React", checked: false },
-    { id:count++, task: "Be awesome!", checked: false },
-    { id:count++, task: "No headaches", checked: false }
+    { id: count++, task: "Learn React", checked: false },
+    { id: count++, task: "Be awesome!", checked: false },
+    { id: count++, task: "No headaches", checked: false }
   ];
   const [todos, setTodos] = useState(initialTodos);
 
+    // Load saved  todos
+    useEffect(() => {
+      const storedTodos= JSON.parse(localStorage.getItem(LSKEY));
+      if (storedTodos) setTodos(storedTodos);
+    }, []);
+
+  // Save todos to localStorage
+  useEffect(() => {
+    window.localStorage.setItem(LSKEY + ".todos", JSON.stringify(todos));
+   
+  }, [todos]);
+
   return (
-    <div className="App"> 
-    <Header/>
-    <Form todos={todos} setTodos={setTodos} count={count}/>
-    <div className="content">
-    <h2>Todos</h2>
-    </div>
-    <List todos={todos} setTodos={setTodos}/>
-    
-    
+    <div className="App">
+      <Header />
+      <Form todos={todos} setTodos={setTodos} count={count} />
+      <div className="content">
+        <h2>Todos</h2>
+      </div>
+      <List todos={todos} setTodos={setTodos} />
+     
+
     </div>
   );
 }
